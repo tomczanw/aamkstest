8df410a3d435ef3303772871005c04949223200d
Tomasz Wróbel <tomek618@o2.pl>
2019-04-05 14:17:50 +0200

    Refactoring

--- StringMethod.py
+++ StringMethod.py
@@ -5,7 +5,7 @@ def length_string(s):
 def count_letter(tab, character):
     counter = 0
     for x in tab:
-        if x == character:
+        #if x == character:
             counter += 1
 
     return counter
